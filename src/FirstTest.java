d5b1edcec8d111cdf231ef6cafef3a38aceaf74b
NishantMud <nishantmudgal39@gmail.com>
2020-03-06 18:08:14 +0530

    My first commit using cmd

---
+++ src/FirstTest.java
@@ -0,0 +1,8 @@
+public class FirstTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello this is first program to push on github");
+        System.out.println("Using IntelliJ and processing it using jenkins");
+    }
+
+}
